e3137eaea1455b86f1257e95130ae4e6c8064cdb
Настя <anastasiiakonyushenko@gmail.com>
2023-05-03 07:50:38 +0300

    updated

---
+++ python_labs/main/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+def index(request):
+    data = {
+        'title': 'Main page?',
+        # 'values': ['some', 'hello', '123'],
+        # 'obj': {
+        #     'car': 'BMW',
+        #     'age': '20',
+        #     'hobby': 'poems'
+        # }
+    }
+    return render(request, 'main/index.html', data)
+
+def about(request):
+    return render(request, 'main/about.html')
