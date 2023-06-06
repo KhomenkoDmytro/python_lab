5d97de8805683c1773a6f1b18e69250bf236add9
Dmytro Khomenko <dmytro081102@gmail.com>
2023-06-06 16:56:20 +0300

    final change

--- python_lab/main/views.py
+++ python_lab/main/views.py
@@ -7,22 +7,33 @@ def index(request):
     appid = '694084e773a14b3e7ca9d68ce25830f0'
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
 
-    if(request.method == 'POST'):
+    if request.method == 'POST':
         form = CityForm(request.POST)
-        form.save()
+        if form.is_valid():
+            city_name = form.cleaned_data['name']
+            existing_city = City.objects.filter(name=city_name).exists()
+            if not existing_city:
+                res = requests.get(url.format(city_name)).json()
+                if res['cod'] == 200:
+                    form.save()
+                else:
+                    form.add_error('name', 'Місто не знайдено')
+            else:
+                form.add_error('name', 'Місто вже додане')
+    else:
+        form = CityForm()
 
-    form = CityForm()
     all_cities = []
     cities = City.objects.all()
     for city in cities:
-       res = requests.get(url.format(city.name)).json()
-       city_info = {
-           'city': city.name,
-           'humidity': res["main"]["humidity"],
-           'temp': res["main"]["temp"],
-           'icon': res["weather"][0]["icon"]
-       }
-       all_cities.append(city_info)
+        res = requests.get(url.format(city.name)).json()
+        city_info = {
+            'city': city.name,
+            'humidity': res["main"]["humidity"],
+            'temp': res["main"]["temp"],
+            'icon': res["weather"][0]["icon"]
+        }
+        all_cities.append(city_info)
 
     data = {
         'title': 'Main page?',
